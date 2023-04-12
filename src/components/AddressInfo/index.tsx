db1bb9ec837335b0dfbf46bf9bd4da8af5623fec
Guilherme Araujo Nascimento <guilherme.nascimento.stefanini@cnpbrasil.com.br>
2023-04-12 01:52:27 -0300

    Feat: AddressForm

--- src/components/AddressInfo/index.tsx
+++ src/components/AddressInfo/index.tsx
@@ -1,18 +1,79 @@
 import { MapPinLine } from "phosphor-react"
 import * as S from "./styles"
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface AddressProps {
+    cep: string;
+    street: string;
+    number: string;
+    complement?: string;
+    district: string;
+    city: string;
+    state: string;
+}
 
 export const AddressInfo = () => {
+    const { register, watch, formState: { errors } } = useForm<AddressProps>();
+    const onSubmit: SubmitHandler<AddressProps> = data => console.log(data);
+
+    console.log(watch("cep"))
+
     return (
         <S.AddressContainer>
-            <MapPinLine/>
-            <S.TitleContent>
-                <S.Title>
-                    Endereço de Entrega
-                </S.Title>
-                <S.SubTitle>
-                    Informe o endereço onde deseja receber seu pedido
-                </S.SubTitle>
-            </S.TitleContent>
+            <S.InfoContainer>
+                <MapPinLine color='#C47F17' size={22} />
+                <S.TitleContent>
+                    <S.Title>
+                        Endereço de Entrega
+                    </S.Title>
+                    <S.SubTitle>
+                        Informe o endereço onde deseja receber seu pedido
+                    </S.SubTitle>
+                </S.TitleContent>
+            </S.InfoContainer>
+            <S.FormAddress>
+                <S.InputAddress 
+                    placeholder="CEP" 
+                    start={{width: "sm"}} 
+                    end={{width: "sm"}} 
+                    {...register("cep", { required: true, maxLength: 8})}
+                />
+                <S.InputAddress 
+                    placeholder="Rua" 
+                    start={{width: "sm"}} 
+                    end={{width: "lg"}} 
+                    {...register("street", { required: true })}/>
+                <S.InputAddress 
+                    placeholder="Numero" 
+                    start={{width: "sm"}} 
+                    end={{width: "sm"}} 
+                    {...register("number", { required: true })}
+                />
+                <S.InputAddress 
+                    placeholder="Complemento" 
+                    start={{width: "md"}} 
+                    end={{width: "lg"}} 
+                    {...register("complement")}
+                    />
+                <S.InputAddress 
+                    placeholder="Bairro" 
+                    start={{width: "sm"}} 
+                    end={{width: "sm"}} 
+                    {...register("district", { required: true })}
+                />
+                <S.InputAddress 
+                    placeholder="Cidade" 
+                    start={{width: "md"}} 
+                    end={{width: "md"}} 
+                    {...register("city", { required: true })}
+                />
+                <S.InputAddress 
+                    placeholder="UF" 
+                    start={{width: "lg"}} 
+                    end={{width: "lg"}} 
+                    {...register("state", { required: true })}
+                />
+            </S.FormAddress>
         </S.AddressContainer>
     )
 }
--- src/components/AddressInfo/styles.ts
+++ src/components/AddressInfo/styles.ts
@@ -1,10 +1,71 @@
 import styled from "styled-components";
 
-// export const Title = styled.h2(({ theme }) => ({
-//     fontFamily: theme.fonts.title,
-//     fontSize: theme.textSizes["title-title-xs"],
-//     color: theme.color["base-subtitle"],
-// }))
+export const InfoContainer = styled.div(({ theme }) => ({
+    display: 'flex',
+}))
+
+export const FormAddress = styled.form(({ theme }) => ({
+    display: 'grid',
+    gridTemplateColumns: '200px 276px 42px',
+    gap: '12px 16px',
+}))
+
+interface InputProps {
+    start: { width: 'sm' | 'md' | 'lg' };
+    end: { width: 'sm' | 'md' | 'lg' };
+}
+
+const InputSizeEnd = ({ end }: InputProps): string => {
+    switch (end.width) {
+        case "sm": {
+            return '2';
+        }
+        case "md": {
+            return '3';
+        }
+        case "lg": {
+            return '4';
+        }
+        default: {
+            return '1';
+        }
+    }
+};
+
+const InputSizeStart = ({ start }: InputProps): string => {
+    switch (start.width) {
+        case "sm": {
+            return '1';
+        }
+        case "md": {
+            return '2';
+        }
+        case "lg": {
+            return '3';
+        }
+        default: {
+            return '1';
+        }
+    }
+};
+
+export const InputAddress = styled.input<InputProps>(({ theme, start, end }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '12px',
+    gap: '4px',
+    background: theme.color["base-input"],
+    borderRadius: '4px',
+    border: `1px solid ${theme.color["base-button"]}`,
+
+    gridColumnStart: InputSizeStart({ start }),
+    gridColumnEnd: InputSizeEnd({ end }),
+
+    '&:focus':{
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    }
+}));
 
 export const AddressContainer = styled.div(({ theme }) => ({
     background: theme.color["base-card"],
